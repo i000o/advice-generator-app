6af501b0474b8aec3fbb54e4ae16d2e672d0e0ef
i000o <iosman1096@gmail.com>
2026-07-09 13:24:07 +0100

    default values to js, complete styles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const advice = document.getElementById("advice"); 
 const dice = document.querySelector('button[type="button"]'); 
+const adviceId = document.getElementById("advice-id"); 
+const adviceGeneratorTitle = document.getElementById("advice-generator-title"); 
+const adviceGenerateInstruction = document.getElementById("advice-generate-instruction"); 
 
 async function fetchAdvice() { 
     const response = await fetch("https://api.adviceslip.com/advice"); 
@@ -12,10 +15,26 @@ async function fetchAdvice() {
 
 function showAdvice(adviceSlip) { 
 
+    advice.hidden = false; 
+    adviceId.hidden = false; 
+    adviceGeneratorTitle.textContent = ""; 
+    adviceGeneratorTitle.classList.remove("title"); 
+    adviceGenerateInstruction.textContent = ""; 
+    adviceGenerateInstruction.classList.remove("instruction"); 
     const quote = adviceSlip.slip.advice; 
     advice.textContent = `${quote}`; 
+    adviceId.textContent = `Advice #${adviceSlip.slip.id}`; 
+}
+
+function defaultValues() { 
+
+    advice.hidden = true; 
+    adviceId.hidden = true; 
+    adviceGeneratorTitle.textContent = "advice generator"; 
+    adviceGeneratorTitle.classList.add("title"); 
+    adviceGenerateInstruction.textContent = "Roll the dice below to receive your advice."; 
+    adviceGenerateInstruction.classList.add("instruction"); 
 }
 
 dice.addEventListener("click", fetchAdvice); 
-
-fetchAdvice(); 
+document.addEventListener("DOMContentLoaded", defaultValues); 
